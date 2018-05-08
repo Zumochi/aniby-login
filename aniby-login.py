8205031591c13b3fcb602db088e90bbe3fb46ddb
Zumochi <zumpyzum@gmail.com>
2018-05-08 16:17:26 +0200

    Hopefully functional login. Requires testing.

--- aniby-login.py
+++ aniby-login.py
@@ -23,70 +23,59 @@ ANIBY_URL = 'https://animebytes.tv'
 REQ_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36'
 REQ_HOST = 'animebytes.tv'
 
-REQ_HEADERS = {
-    'User-Agent':   REQ_AGENT,
-    'Host':         REQ_HOST,
+HEADERS = {
+    'User-Agent': REQ_AGENT,
+    'Host': REQ_HOST,
 }
-
 
 session = requests.Session()
 
 
 def get_auth_info(auth_file):
     try:
-        with open(arguments['FILE'], 'r') as auth_file:
+        with open(auth_file, 'r') as f:
             try:
-                auth_data = yaml.safe_load(auth_file)
+                auth_data = yaml.safe_load(f)
                 if not auth_data:
                     error("Authentication file empty!")
                     sys.exit(1)
-                username = auth_data['username']
-                password = auth_data['password']
+                return auth_data
             except yaml.YAMLError as ymlerr:
                 error('Loading yaml authentication file failed: {ymlerr}'.format(ymlerr=ymlerr))
                 sys.exit(1)
     except EnvironmentError as ee:
-        error("Couldn't load authentication file {auth_file}".format(auth_file=auth_file))
+        error("Couldn't load authentication file {filename}: {error}".format(filename=ee.filename, error=ee.strerror))
         sys.exit(1)
-
-    return {
-        'username': username,
-        'password': password,
-    }
 
 
 def get_csrf_data(html):
-    soup = BeautifulSoup(html)
-    csrf_index = soup.find('input', type='hidden', name='_CSRF_INDEX')
-    csrf_token = soup.find('input', type='hidden', name='_CSRF_TOKEN')
-    print()
+    soup = BeautifulSoup(html, 'lxml')
+    token = soup.find('input', {'name': '_CSRF_TOKEN'})
+    index = soup.find('input', {'name': '_CSRF_INDEX'})
+
+    return {'token': token['value'], 'index': index['value']}
 
 
-def aniby_login(auth_info):
+def aniby_login(auth):
+    prv_url = ANIBY_URL
     req_url = ANIBY_URL + '/user/login'
-    req_aniby_login_page = session.get(req_url, headers=REQ_HEADERS)
-    print(req_aniby_login_page.url)
-    print('Headers')
-    print(req_aniby_login_page.headers)
-    print('Cookies')
-    print(req_aniby_login_page.cookies)
+    req_aniby_login_page = session.get(req_url, headers=HEADERS)
+    csrf_data = get_csrf_data(req_aniby_login_page.content)
 
-    csrf_index = False
-    csrf_token = False
+    req_headers = HEADERS
+    req_headers['Referer'] = prv_url
 
-    REQ_HEADERS['Referer'] = ANIBY_URL
+    req_params = {'username': auth['username'],
+                  'password': auth['password'],
+                  'login': 'Log+In!',
+                  '_CSRF_INDEX': csrf_data['index'],
+                  '_CSRF_TOKEN': csrf_data['token']}
+    req_cookies = {'__cfduid': req_aniby_login_page.cookies['__cfduid'],
+                   'transient': req_aniby_login_page.cookies['transient']}
 
-    REQ_COOKIES = {
-        '__cfduid': req_aniby_login_page.cookies['__cfduid'],
-        'transient': req_aniby_login_page.cookies['transient'],
-    }
-    REQ_PARAMS = {
-        'username': auth_info['username'],
-        'password': auth_info['password'],
-        'login':    'Log+In!',
-        '_CSRF_INDEX':  csrf_index,
-        '_CSRF_TOKEN':  csrf_token,
-    }
+    post_login = session.post(req_url, data=req_params, headers=req_headers)
+    print(post_login.text)
+    print(post_login.status_code)
 
 
 if __name__ == '__main__':
